b1e2130eaef44eda6d5cade3f86054cc4e7c0392
danial <dev.salahi@gmail.com>
2021-02-15 23:32:27 +0330

    using navbar in the app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+
+import Navbar from './components/navbar'
+
+// import './App.css';
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Navbar />
+
+    </>
   );
 }
 
