1dae0d6df5299b1332fa0af7fbd009bee532839a
danial <dev.salahi@gmail.com>
2021-02-19 16:17:46 +0330

    implemented the auth service class

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,35 @@
+import jwtDecode from 'jwt-decode'
+
+export class AuthService {
+
+    private tokenKey: string
+    constructor() {
+        this.tokenKey = 'token'
+    }
+
+    login(): void {
+
+    }
+
+    loginWithJwt(jwt: string) {
+        localStorage.setItem(this.tokenKey, jwt)
+    }
+
+    register(): void {
+
+    }
+
+    logout(): void {
+        localStorage.removeItem(this.tokenKey)
+    }
+
+    getCurrentUser() {
+        try {
+            const jwt: string = localStorage.getItem(this.tokenKey) as string
+            return jwtDecode(jwt)
+        }
+        catch (ex) {
+            return null
+        }
+    }
+}
