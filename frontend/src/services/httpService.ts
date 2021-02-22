a2aeaf3a87ea4fdb78def224a79581f3df74ca7e
danial <dev.salahi@gmail.com>
2021-02-22 11:21:31 +0330

    frontend: implemented the setHeaders method in httpService class to sending JWT to the backend

--- frontend/src/services/httpService.ts
+++ frontend/src/services/httpService.ts
@@ -1,12 +1,19 @@
 import axios, { AxiosInstance } from 'axios'
-
+import { AuthService } from './authService'
 export class HttpService {
 
     private apiEndPoint: string = ``
+    private authService: AuthService
     private client: AxiosInstance
 
     constructor() {
         this.client = axios.create({ baseURL: this.apiEndPoint, headers: {} })
+        this.authService = new AuthService()
+        this.setHeaders()
+    }
+
+    setHeaders(): void {
+        axios.defaults.headers.common['x-auth-token'] = this.authService.getJwt()
     }
 
     get() {
