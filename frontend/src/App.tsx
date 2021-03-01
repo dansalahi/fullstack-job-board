bddc86180b65939820e22d35cf10c47949121ff2
danial <dev.salahi@gmail.com>
2021-03-01 21:17:53 +0330

    frontend: using smooth scrool component in the App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from './components/navbar'
 import FixVideo from './components/video/video'
 // import './App.css';
+import SmoothScroll from "./components/common/smoothScroll/smoothScroll";
 
 function App() {
   return (
-    <>
+    <SmoothScroll>
       <FixVideo />
       {/* <Navbar /> */}
-    </>
+    </SmoothScroll>
   );
 }
 
