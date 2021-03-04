5c8249c1b0098a2166ae3096adfbc9b6eab8a805
danial <dev.salahi@gmail.com>
2021-03-04 21:00:27 +0330

    frontend: fixed video url by process.env

--- frontend/src/components/video/video.tsx
+++ frontend/src/components/video/video.tsx
@@ -3,7 +3,7 @@ import classes from './video.module.css'
 const Video: React.FC = () => {
     return (
         <video autoPlay loop muted className={classes.typingVideo}>
-            <source src="/assets/video/LaptopBrowsing.mp4" type="video/mp4" />
+            <source src={process.env.PUBLIC_URL + '/assets/video/LaptopBrowsing.mp4'} type="video/mp4" />
         </video >
     );
 }
