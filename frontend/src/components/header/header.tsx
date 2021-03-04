2ed67c3dc4fd5b99cd7b95f2eb3f3d0be86cd9e0
danial <dev.salahi@gmail.com>
2021-03-04 21:01:11 +0330

    frontend: implement header component

---
+++ frontend/src/components/header/header.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react'
+
+import { makeStyles } from '@material-ui/core/styles'
+import { Typography, IconButton, Collapse } from '@material-ui/core'
+import { ExpandMore as ExpandMoreIcon } from '@material-ui/icons'
+
+
+// Navbar
+import Navbar from './navbar'
+
+
+const useStyles = makeStyles(theme => ({
+
+
+    header: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+    },
+
+    headerTitle: {
+        fontSize: '4.5rem',
+        fontWeight: 'bold',
+        color: '#30336b',
+        textAlign: 'center'
+    },
+
+    container: {
+        textAlign: 'center'
+    },
+
+
+    exapandMore: {
+        color: '#d35400',
+        fontSize: '5rem'
+    },
+
+}))
+
+
+export default function Header() {
+
+    const classes = useStyles()
+
+    const [check, setCheck] = useState(false)
+
+    useEffect(() => {
+        setCheck(true)
+    }, [])
+
+    return (
+        <header className={classes.header}>
+            <Navbar />
+            <Collapse in={check} collapsedHeight={50} {...(check ? { timeout: 1000 } : {})} >
+                <div className={classes.container}>
+                    <Typography className={classes.headerTitle} variant="h1" align="center" gutterBottom noWrap>10,253, Job Listed Here!</Typography>
+                    <IconButton>
+                        <ExpandMoreIcon className={classes.exapandMore} />
+                    </IconButton>
+                </div>
+            </Collapse>
+        </header>
+    )
+
+
+}
