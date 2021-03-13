148a94ea92535593ca4dc4c4b3e3c1809dca1ce2
danial <dev.salahi@gmail.com>
2021-03-13 20:57:52 +0330

    removed import React from header

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
 import { Typography, IconButton, Collapse } from '@material-ui/core'
