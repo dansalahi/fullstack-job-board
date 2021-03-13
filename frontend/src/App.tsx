af5057e27478c729f2b79194a591761b9b3cfdba
danial <dev.salahi@gmail.com>
2021-03-13 21:00:34 +0330

    import component listings into app main component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
 import FixVideo from './components/video/video'
 import SmoothScroll from "./components/common/smoothScroll/smoothScroll";
-import { CssBaseline } from '@material-ui/core'
+import { CssBaseline, Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import Header from './components/header/header';
+import Listings from './components/listing/listings'
+
+import Avatars from './components/header/avatars'
 
 const useStyles = makeStyles(theme => ({
 
@@ -23,6 +25,8 @@ function App() {
       <div className={classes.root}>
         <CssBaseline />
         <Header />
+        <Listings />
+
       </div>
     </>
   );
