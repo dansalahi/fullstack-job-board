c177ca1dced6827fe073c24a36ce23be6810dfaf
danial <dev.salahi@gmail.com>
2021-04-12 23:10:20 +0300

    change the css for video component

--- frontend/src/components/video/video.tsx
+++ frontend/src/components/video/video.tsx
@@ -1,11 +1,14 @@
-import classes from './video.module.css'
+import "./video.css";
 
 const Video: React.FC = () => {
-    return (
-        <video autoPlay loop muted className={classes.typingVideo}>
-            <source src={process.env.PUBLIC_URL + '/assets/video/LaptopBrowsing.mp4'} type="video/mp4" />
-        </video >
-    );
-}
+  return (
+    <video autoPlay loop muted className="typingVideo">
+      <source
+        src={process.env.PUBLIC_URL + "/assets/video/LaptopBrowsing.mp4"}
+        type="video/mp4"
+      />
+    </video>
+  );
+};
 
 export default Video;
