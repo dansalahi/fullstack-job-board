ee8241d35807a4d45a63e5e8d73717a6c22bbfe6
danial <dev.salahi@gmail.com>
2021-04-24 22:13:31 +0300

    added react-toastify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,30 @@
 import FixVideo from './components/video/video'
-import SmoothScroll from "./components/common/smoothScroll/smoothScroll";
+import SmoothScroll from './components/common/smoothScroll/smoothScroll'
 import { CssBaseline, Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import Header from './components/header/header';
+import Header from './components/header/header'
 import Listings from './components/listing/listings'
-
 import Avatars from './components/header/avatars'
 
-const useStyles = makeStyles(theme => ({
+// react toastify
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
+const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: '100vh',
-  }
-
+  },
 }))
 
 function App() {
-
-  const classes = useStyles();
-
   return (
     <>
-      <div className={classes.root}>
-        <CssBaseline />
-        <Header />
-        <Listings />
-
-      </div>
+      <ToastContainer />
+      <CssBaseline />
+      <Header />
+      <Listings />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
