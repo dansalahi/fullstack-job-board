58ccbadad4b016c24bb69c3a05d4e904c9b9b9a8
danial <dev.salahi@gmail.com>
2021-04-24 22:48:33 +0300

    get ride of bidirectinal depednecy

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,39 +1,47 @@
+import { HttpService } from './httpService'
+import { apiUrl } from './../config.json'
 import jwtDecode from 'jwt-decode'
-
 export class AuthService {
+  private tokenKey: string
+  private apiEndpoint: string
+  private http: HttpService
 
-    private tokenKey: string
-    constructor() {
-        this.tokenKey = 'token'
+  constructor() {
+    this.http = new HttpService()
+    this.http.setJwt(this.getJwt())
+
+    this.tokenKey = 'token'
+    this.apiEndpoint = apiUrl + '/auth'
+  }
+
+  loginWithJwt(jwt: string) {
+    localStorage.setItem(this.tokenKey, jwt)
+  }
+
+  async login() {
+    // const { data: jwt } = await this.http.post(this.apiEndpoint, {
+    //   email: string,
+    //   password: string,
+    // })
+    // localStorage.setItem(this.tokenKey, jwt)
+  }
+
+  register(): void {}
+
+  logout(): void {
+    localStorage.removeItem(this.tokenKey)
+  }
+
+  getCurrentUser() {
+    try {
+      const jwt: string = localStorage.getItem(this.tokenKey) as string
+      return jwtDecode(jwt)
+    } catch (ex) {
+      return null
     }
+  }
 
-    login(): void {
-
-    }
-
-    loginWithJwt(jwt: string) {
-        localStorage.setItem(this.tokenKey, jwt)
-    }
-
-    register(): void {
-
-    }
-
-    logout(): void {
-        localStorage.removeItem(this.tokenKey)
-    }
-
-    getCurrentUser() {
-        try {
-            const jwt: string = localStorage.getItem(this.tokenKey) as string
-            return jwtDecode(jwt)
-        }
-        catch (ex) {
-            return null
-        }
-    }
-
-    getJwt(): string {
-        return localStorage.getItem(this.tokenKey) as string
-    }
+  getJwt(): string {
+    return localStorage.getItem(this.tokenKey) as string
+  }
 }
